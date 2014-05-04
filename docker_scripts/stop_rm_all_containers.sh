469f6e466800a80922196853176809dd181bbd4a
Robert Berger <robert.berger@ReliableEmbeddedSystems.com>
2014-05-04 14:12:20 +0300

    more verbosity

--- docker_scripts/stop_rm_all_containers.sh
+++ docker_scripts/stop_rm_all_containers.sh
@@ -1,2 +1,8 @@
+echo "+ docker ps -a"
+docker ps -a
+echo "+ docker stop $(docker ps -a -q)"
 docker stop $(docker ps -a -q)
+echo "+ docker rm $(docker ps --no-trunc -a -q)"
 docker rm $(docker ps --no-trunc -a -q)
+echo "+ docker ps -a"
+docker ps -a

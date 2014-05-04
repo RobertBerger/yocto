0ce9c3edb9f18e85021106e54f4ba2f47a1b07b8
Robert Berger <robert.berger@ReliableEmbeddedSystems.com>
2014-05-04 16:15:44 +0300

    attempt to share files between host/container to avoid docker commits

--- local_scripts/docker_run.sh
+++ local_scripts/docker_run.sh
@@ -1,2 +1,24 @@
-docker run -i -t reslocal/yocto /bin/bash
-#docker run -i -t reslocal/yocto run && /bin/bash
+IMAGE_NAME="reslocal/yocto"
+HOST_FOLDER="${HOME}/yocto-image"
+IMAGE_FOLDER="/home/genius/yocto"
+# this is a hack against permission denied
+if [ ! -d ${HOST_FOLDER} ];
+then
+     echo "${HOST_FOLDER} does not exist, creating it"
+     mkdir -p ${HOST_FOLDER}
+     chmod a+w ${HOST_FOLDER}
+else
+     echo "${HOST_FOLDER} exists"
+fi
+#docker run -i -t reslocal/yocto /bin/bash
+#echo "+ ID=\$(docker run -p 127.0.0.1::22 -i -t -d ${IMAGE_NAME} /bin/bash)"
+#ID=$(docker run -p 127.0.0.1::22 -i -t -d ${IMAGE_NAME} /bin/bash)
+#echo "+ ID=\$(docker run -i -t -d ${IMAGE_NAME} /bin/bash)"
+#ID=$(docker run -i -t -d ${IMAGE_NAME} /bin/bash)
+echo "+ ID=\$(docker run -i -t -d -v ${HOST_FOLDER}:${IMAGE_FOLDER}:rw ${IMAGE_NAME} /bin/bash)"
+ID=$(docker run -i -t -d -v ${HOST_FOLDER}:${IMAGE_FOLDER}:rw ${IMAGE_NAME} /bin/bash)
+#echo "+ IPAddress: $(docker inspect --format '{{ .NetworkSettings.IPAddress }}' ${ID})"
+# ssh does not work for now
+#echo "+ ssh to: $(docker port ${ID} 22)"
+echo "+ docker attach ${ID}"
+docker attach ${ID}
--- non-local_scripts/docker_run.sh
+++ non-local_scripts/docker_run.sh
@@ -1,2 +1,18 @@
-docker run -i -t reliableembeddedsystems/yocto /bin/bash
-#docker run -i -t reliableembeddedsystems/yocto run && /bin/bash
+IMAGE_NAME="reslocal/yocto"
+HOST_FOLDER="${HOME}/yocto-image"
+IMAGE_FOLDER="/home/genius/yocto"
+# this is a hack against permission denied
+if [ ! -d ${HOST_FOLDER} ];
+then
+     echo "${HOST_FOLDER} does not exist, creating it"
+     mkdir -p ${HOST_FOLDER}
+     chmod a+w ${HOST_FOLDER}
+else
+     echo "${HOST_FOLDER} exists"
+fi
+# let's run it - we'll share a folder with the host
+echo "+ ID=\$(docker run -i -t -d -v ${HOST_FOLDER}:${IMAGE_FOLDER}:rw ${IMAGE_NAME} /bin/bash)"
+ID=$(docker run -i -t -d -v ${HOST_FOLDER}:${IMAGE_FOLDER}:rw ${IMAGE_NAME} /bin/bash)
+# let's attach to it
+echo "+ docker attach ${ID}"
+docker attach ${ID}

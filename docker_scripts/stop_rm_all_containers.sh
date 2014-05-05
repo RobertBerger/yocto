ecbf73f587d3005f1823e7c11728811e394f8919
Robert Berger <robert.berger@ReliableEmbeddedSystems.com>
2014-05-05 22:01:41 +0300

    rm -f

--- docker_scripts/stop_rm_all_containers.sh
+++ docker_scripts/stop_rm_all_containers.sh
@@ -2,7 +2,7 @@ echo "+ docker ps -a"
 docker ps -a
 echo "+ docker stop $(docker ps -a -q)"
 docker stop $(docker ps -a -q)
-echo "+ docker rm $(docker ps --no-trunc -a -q)"
-docker rm $(docker ps --no-trunc -a -q)
+echo "+ docker rm -f $(docker ps --no-trunc -a -q)"
+docker rm -f $(docker ps --no-trunc -a -q)
 echo "+ docker ps -a"
 docker ps -a
